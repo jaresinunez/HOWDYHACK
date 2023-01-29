5924f7870fdaf09cb90470aeba7d0bae8c064c0f
Alish Sharma <75708780+alishsharma17@users.noreply.github.com>
2023-01-28 18:54:04 -0600

    working sign up

--- app/src/main/java/com/jaam/howdyhack/MainActivity.kt
+++ app/src/main/java/com/jaam/howdyhack/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.jaam.howdyhack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jaam.howdyhack.databinding.ActivityMainBinding
+import javax.net.ssl.SSLSessionBindingEvent
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signButton.setOnClickListener {
+            val i = Intent(this, signpactivity::class.java)
+            startActivity(i)
+            finish()
+        }
     }
 }
