022bcf1792243e4aafc88df37b4981672950116a
melanie <melaniejnunez@gmail.com>
2023-01-29 03:45:39 -0600

    button functions added

--- app/src/main/java/com/jaam/howdyhack/WelcomeScreenActivity.kt
+++ app/src/main/java/com/jaam/howdyhack/WelcomeScreenActivity.kt
@@ -1,5 +1,6 @@
 package com.jaam.howdyhack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jaam.howdyhack.databinding.ActivityWelcomeScreenBinding
@@ -10,5 +11,11 @@ class WelcomeScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityWelcomeScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.continueButton?.setOnClickListener {
+            val i = Intent(this, DashboardActivity::class.java)
+            startActivity(i)
+            finish()
+        }
     }
 }
